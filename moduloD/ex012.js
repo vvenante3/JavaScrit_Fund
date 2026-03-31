5f0cc378d7b8b8c9df7d0909fedac454eeb8c2c2
Vitor Acer5 <vitor.venante@gmail.com>
2026-03-31 19:30:52 -0300

    finalização introdução JS - Curso em Vídeo

---
+++ moduloD/ex012.js
@@ -0,0 +1,28 @@
+// Elaborar desafio "Dia da Semana"
+
+var agora = new Date()
+var diaSemana = agora.getDay()
+
+switch(diaSemana){
+    case 0: 
+        console.log(`Hoje é Domingo, é.. tá bão..`)
+    break
+    case 1:
+        console.log(`Hoje é Segunda-Feira, aff`)
+    break
+    case 2:
+        console.log(`Hoje é Terça-Feira, melhor do que nada!`)
+    break
+    case 3:
+        console.log(`Hoje é Quarta-Feira, ok`)
+    break
+    case 4:
+        console.log(`Hoje é Quinta-Feira, tá quase!`)
+    break
+    case 5:
+        console.log(`Hoje é Sexta-Feira Ihaaaaa!`)
+    break
+    case 6:
+        console.log(`Hoje é Sábado, Ueeepaaa!!`)
+
+}
