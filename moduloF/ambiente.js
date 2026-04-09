f9d10c2b35a2d34ea1f3acc3cf9a797355ce30f6
Vitor Acer5 <vitor.venante@gmail.com>
2026-04-09 20:51:26 -0300

    variávies compostos -> vetores

---
+++ moduloF/ambiente.js
@@ -0,0 +1,5 @@
+let num = [5,8,2,9,3]
+num.sort()
+
+console.log(`O vetor tem ${num.length} elementos`)
+console.log(num)
---
+++ moduloF/vetorNaTela.js
@@ -0,0 +1,9 @@
+let valores = [8, 1, 7, 4, 2,9]
+
+// for(let pos = 0; pos <valores.length; pos++){
+//     console.log(valores[pos])
+// }
+
+for(let pos in valores){              // valores compostos e vetores
+    console.log(valores[pos])
+}
